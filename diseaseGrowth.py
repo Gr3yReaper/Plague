79d6b66a099dc7881be682418292df177ab89eb7
Gr3yReaper <cl.phillips19@googlemail.com>
2023-01-26 15:17:51 +0000

    Adjusted diseaseGrowth to have fixed active members at 0.7 to further algorithm

--- diseaseGrowth.py
+++ diseaseGrowth.py
@@ -1,6 +1,9 @@
 import math
 
+# Will keep healthy members for each country for now as a fixed 0.7, might need to adjust later on
+ACTIVE_PEOPLE = 0.7
+
 
 def update(current, activityLevel):
-    current = current * activityLevel
+    current = current * activityLevel * ACTIVE_PEOPLE
     return math.ceil(current)
