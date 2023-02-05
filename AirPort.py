187f20b94cdd3e24a54928fa6fa5fd8274fd7e4d
Gr3yReaper <cl.phillips19@googlemail.com>
2023-02-05 14:52:04 +0000

    Airport has been made to be able to infect other countries with airports, numbers need tweaking for infecting as Russia despite starting later than UK makes it to 10k first most times. Other than that working as expected

--- AirPort.py
+++ AirPort.py
@@ -1,3 +1,6 @@
+import random
+
+
 class AirPort:
     def __init__(self, country, destinations, activity):
         self.country = country
@@ -18,3 +21,12 @@ class AirPort:
 
     def update_activity(self, activity):
         self.activity = activity
+
+    def flight(self, infected):
+        if infected > 0:
+            chance = random.randint(1, 100)
+            if chance <= self.activity:
+                country_index = random.randint(0, len(self.destinations) - 1)
+                return self.destinations[country_index]
+        return None
+
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import diseaseGrowth
 from Country import *
 from AirPort import *
 
-BASE_ACTIVITY = 1
+BASE_ACTIVITY = 80
 AIRPORT_COUNTRIES = ["UK", "Russia"]
 AIRPORT_DETAILS = [["UK", ["Russia"], BASE_ACTIVITY], ["Russia", ["UK"], BASE_ACTIVITY]]
 PORT_COUNTRIES = ["UK", "Russia"]
@@ -10,7 +10,8 @@ PORT_COUNTRIES = ["UK", "Russia"]
 countries = [Country("UK", 6733000000, 1, Activity.SLIGHT, Awareness.SLIGHT,
                      Protective.LITTLE, Medical.HIGH),
              Country("Russia", 1434000000, 0, Activity.UNRESTRICTED, Awareness.UNAWARE,
-                     Protective.NONE, Medical.ADVANCED)]
+                     Protective.MANDATORY, Medical.ADVANCED)]
+airport_list = []
 
 for x in range(len(countries)):
     if countries[x].get_name() in AIRPORT_COUNTRIES:
@@ -19,20 +20,27 @@ for x in range(len(countries)):
         individual = AIRPORT_DETAILS[location]
         airport = AirPort(individual[0], individual[1], individual[2])
         countries[x].set_airport(airport)
+        airport_list.append(countries[x])
 
 while not any([n.get_infected() >= 10000 for n in countries]):
     # Calculates the new infected numbers for each country then updates the objects
+    # Work out rough chance of said airport travelling, randomly pick location etc
+    flight_log = ([n.get_airport().flight(n.get_infected()) for n in airport_list])
     newInfected = [diseaseGrowth.update(n.get_infected(), n.get_activity().value,
                                         n.get_awareness().value, n.get_protective().value,
                                         n.get_medical().value) for n in countries]
-
     # Modified algorithm to add people as infected
     for x in range(len(countries)):
         countries[x].update_infected(newInfected[x])
 
+    for x in range(len(flight_log)):
+        if flight_log[x] is not None:
+            for y in range(len(countries)):
+                if countries[y].get_name() == flight_log[x] and countries[y].get_infected() == 0:
+                    countries[y].update_infected(1) # Set as 1 for now to start infection
+
     # One airport/ boat per country for now
     # Random chance one will start (pick a random country with a free port/ airport
     # around 0.8% when unaware, 0.5 when slight, 0.3 when aware, 0.1 when reduced, and 0 for lockdown rough values
-
-print([n.get_infected() for n in countries])
+    print([n.get_infected() for n in countries])
 print("out of loop")
