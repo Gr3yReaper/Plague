ab0f689ea1d560803dbf213f550f31afbb054311
Gr3yReaper <cl.phillips19@googlemail.com>
2023-02-06 23:03:03 +0000

    Created parent port class, implemented methods to be inherrited by all child classes.

--- Port.py
+++ Port.py
@@ -3,3 +3,21 @@ class Port:
         self.country = country
         self.destinations = destinations
         self.activity = activity
+
+    def get_country(self):
+        return self.country
+
+    def get_destination(self):
+        return self.destinations
+
+    def get_activity(self):
+        return self.activity
+
+    def set_country(self, country):
+        self.country = country
+
+    def set_destinations(self, destinations):
+        self.destinations = destinations
+
+    def set_activity(self, activity):
+        self.activity = activity
